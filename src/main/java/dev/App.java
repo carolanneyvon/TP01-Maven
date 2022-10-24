26898296b2b999efface80f013f9155f90825bc5
Carolanne Yvon <elolithia@gmail.com>
2022-10-24 14:21:03 +0200

    git init

---
+++ src/main/java/dev/App.java
@@ -0,0 +1,16 @@
+package dev;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String asciiArt = FigletFont.convertOneLine("Hello World!");
+        System.out.println(asciiArt);
+    }
+}
